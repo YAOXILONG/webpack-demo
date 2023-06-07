f25e6baf35952dbc74f5d7da3b2afdc40a56f331
yaoxilong <yaoxilong@hikvision.com.cn>
2023-06-07 20:41:00 +0800

    自动编译

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './0.jpg'
+import printMe from './print.js';
 function component () {
  const element = document.createElement('div')
- element.innerHTML = _.join(['Hello','webpack'],'')
- element.classList.add('hello')
- const myIcon = new Image()
- myIcon.src = Icon
- element.appendChild(myIcon)
+ const btn = document.createElement('button')
+ element.innerHTML = _.join(['Hello','webpack'],' ')
+ btn.innerHTML = 'Click me and check the console!'
+ btn.onclick = printMe
+ element.appendChild(btn)
  return element
 }
 document.body.appendChild(component())
